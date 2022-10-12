6d39ff2b3ffc77dd83bb932fff8dc9fd21a46752
Buket Soyhan <buketsoyhan1998@outlook.com>
2022-10-12 20:48:41 +0300

    State, setState

--- react-examples/examples/src/App.js
+++ react-examples/examples/src/App.js
@@ -1,21 +1,37 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import User from './components/User';
-
-function App() {
-  return (
-    <div className="container">
-      <Navbar title="User App"/>
-      <User name="Buket Soyhan"
-      salary="12000"
-      department="Development"
-      />
-      <User name="Batuhan Soyhan"
-      salary="10000"
-      department="Education"
-      />
-    </div>
-  );
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import Users from "./components/Users";
+import "./App.css"
+export default class App extends Component {
+  state = {
+    users: [
+      {
+        id: 1,
+        name: "Buket Soyhan",
+        salary: "15200",
+        department: "IT",
+      },
+      {
+        id: 2,
+        name: "Batuhan Soyhan",
+        salary: "12200",
+        department: "Education",
+      },
+      {
+        id: 3,
+        name: "Aslhan Aslan",
+        salary: "19200",
+        department: "Development",
+      },
+    ],
+  };
+  render() {
+    return (
+      <div className="container">
+        <Navbar title="User App" />
+        <hr/>
+        <Users users={this.state.users}/>
+      </div>
+    );
+  }
 }
-
-export default App;
--- react-examples/examples/src/components/User.js
+++ react-examples/examples/src/components/User.js
@@ -2,11 +2,14 @@ import React, { Component } from "react";
 
 export default class User extends Component {
   state = {
-    isVisible: true,
+    isVisible: false,
   };
-  onClickEvent(e){
-    console.log(this);
+  onClickEvent=(e)=>{
+    this.setState ( {
+      isVisible : !this.state.isVisible
+    })
   }
+
   render() {
     //DESTRUCTING
     const { name, department, salary } = this.props;
@@ -15,7 +18,7 @@ export default class User extends Component {
       <div className="col-md-8 mb-4">
         <div className="card">
           <div className="card-header d-flex justify-content-between">
-            <h4 className="d-inline" onClick={this.onClickEvent.bind(this)} >{name}</h4>
+            <h4 className="d-inline" onClick={this.onClickEvent} >{name}</h4>
 
             <i className="far fa-trash-alt" style={{ cursor: "pointer" }}></i>
           </div>
