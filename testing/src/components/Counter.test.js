a10b088b2ed27d586e183ab28054322f6e372f36
Buket Soyhan <buketsoyhan1998@outlook.com>
2022-11-07 03:03:25 +0300

    Counter Test

--- testing/src/components/Counter.test.js
+++ testing/src/components/Counter.test.js
@@ -1,13 +1,37 @@
-import {render, screen} from "@testing-library/react"
-import userEvent from "@testing-library/user-event"
-import Counter from "./Counter"
+import {render, screen} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Counter from "./index";
 
-test("increase btn", ()=>{
-    render(<Counter/>)
+describe("Counter Tests", () => {
+    let increaseBtn, decreaseBtn, count;
 
-    const increaseBtn = screen.getByText("Inrement")
+    beforeEach(() => {
+        console.log("Her testten önce çalışacağım!");
+        render(<Counter />);
+        increaseBtn = screen.getByText("Increase");
+        decreaseBtn = screen.getByText("Decrease");
+        count = screen.getByText("0");
+    });
 
-    userEvent.click(increaseBtn)
-})
+    beforeAll(() => {
+        console.log("İlk başta bir kere çalışacağım!");
+    });
 
+    afterEach(() => {
+        console.log("Her testten sonra çalışacağım!");
+    });
 
+    afterAll(() => {
+        console.log("En son bir kere çalışacağım!");
+    });
+
+    test("increase btn", () => {
+        userEvent.click(increaseBtn);
+        expect(count).toHaveTextContent("1");
+    });
+
+    test("decrease btn", () => {
+        userEvent.click(decreaseBtn);
+        expect(count).toHaveTextContent("-1");
+    });
+});
